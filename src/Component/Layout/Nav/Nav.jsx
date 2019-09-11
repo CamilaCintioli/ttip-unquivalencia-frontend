cc826e92206368654d7868eb7f119bb3b19234ab
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-09-11 17:47:14 -0300

    logo

--- src/Component/Layout/Nav/Nav.jsx
+++ src/Component/Layout/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from './logo.png';
+import Logo from './logoGood.png';
 import './Nav.css';
 
 function Nav() {
@@ -8,6 +8,7 @@ function Nav() {
     <nav className="navbar navbar-expand-xl navbar-light bg-light">
       <Link className="navbar-brand" to="/">
         <img className="nav-logo" src={Logo} alt="Logo" />
+        UNQuivalencias
       </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
