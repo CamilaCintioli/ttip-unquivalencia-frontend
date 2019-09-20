779a56f8ae2ca3895d34e462cefcc7ef71fed0be
Fernando Nahuel Autalan <f.n.autalan@gmail.com>
2019-09-20 10:48:53 -0300

    refactor reducers search

--- src/redux/reducers/search.js
+++ src/redux/reducers/search.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unreachable */
 import { get } from 'lodash';
 import {
   SEARCH_FILE_START, SEARCH_FILE_ERROR, SEARCH_FILE_COMPLETE, SEARCH_REQUEST_START,
@@ -12,31 +11,22 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case SEARCH_FILE_START:
       return { ...state, isLoading: true, file: null };
-      break;
     case SEARCH_FILE_COMPLETE:
       return { ...state, isLoading: false, fileResult: get(action, 'results.data') };
-      break;
     case SEARCH_FILE_ERROR:
       return { ...state, isLoading: false, file: null };
-      break;
     case SEARCH_REQUEST_START:
       return { ...state, isLoadingRequest: true, request: null };
-      break;
     case SEARCH_REQUEST_COMPLETE:
       return { ...state, isLoadingRequest: false, requestResult: get(action, 'results.data') };
-      break;
     case SEARCH_REQUEST_ERROR:
       return { ...state, isLoadingRequest: false, request: null };
-      break;
     case SEARCH_REQUEST_BY_ID_START:
       return { ...state, isLoading: true, requestResult: null };
-      break;
     case SEARCH_REQUEST_BY_ID_COMPLETE:
       return { ...state, isLoading: false, movieResult: get(action, 'results.data') };
-      break;
     case SEARCH_REQUEST_BY_ID_ERROR:
       return { ...state, isLoading: false, movieResult: null };
-      break;
     default:
       return { ...state };
   }
