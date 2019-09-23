1680d8b3c086d61cec08a3c6d4daf593d00f09df
camilacintioli <camila.cintioli@gmail.com>
2019-09-23 15:28:19 -0300

    Agrego acciones para aprobar y rechazar equivalencias

--- src/Component/Request/RequestPage.jsx
+++ src/Component/Request/RequestPage.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { shape, string } from 'prop-types';
 import RequestDisplay from './RequestDisplay';
 import { searchRequestById } from '../../redux/actions/search';
+import { approveEquivalence, rejectEquivalence } from '../../redux/actions/updateEquivalence';
 
 export default function RequestPage(props) {
   const dispatch = useDispatch();
   const request = useSelector((state) => state.search.request);
+  const giveEquivalence = useCallback(() => {
+    dispatch(approveEquivalence({ requestId: props.match.params.solicitudId }));
+  }, [dispatch]);
+  const denyEquivalence = useCallback(() => {
+    dispatch(rejectEquivalence({ requestId: props.match.params.solicitudId }));
+  }, [dispatch]);
   useEffect(() => {
     dispatch(searchRequestById({ requestId: props.match.params.solicitudId }));
-  }, [searchRequestById]);
-  return (<RequestDisplay request={request} />);
+  }, [dispatch]);
+  return (
+    <RequestDisplay
+      request={request}
+      onEquivalenceGiven={giveEquivalence}
+      onEquivalenceDenied={denyEquivalence}
+    />
+  );
 }
 
 RequestPage.propTypes = {
---
+++ src/redux/reducers/updateEquivalence.js
@@ -0,0 +1,25 @@
+import {
+  APPROVE_EQUIVALENCE_START, APPROVE_EQUIVALENCE_COMPLETE, APPROVE_EQUIVALENCE_ERROR,
+  REJECT_EQUIVALENCE_START, REJECT_EQUIVALENCE_COMPLETE, REJECT_EQUIVALENCE_ERROR,
+} from '../../consts/actionTypes';
+
+const initialState = {};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case APPROVE_EQUIVALENCE_START:
+      return { ...state };
+    case APPROVE_EQUIVALENCE_COMPLETE:
+      return { ...state };
+    case APPROVE_EQUIVALENCE_ERROR:
+      return { ...state };
+    case REJECT_EQUIVALENCE_START:
+      return { ...state };
+    case REJECT_EQUIVALENCE_COMPLETE:
+      return { ...state };
+    case REJECT_EQUIVALENCE_ERROR:
+      return { ...state };
+    default:
+      return { ...state };
+  }
+}
