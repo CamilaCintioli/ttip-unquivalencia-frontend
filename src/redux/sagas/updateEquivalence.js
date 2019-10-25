e5487c3727f36b39c45f2eeee6ef49862e76e5f0
camilacintioli <camila.cintioli@gmail.com>
2019-10-24 23:02:02 -0300

    Creo accion en redux para consultar una equivalencia

--- src/redux/sagas/updateEquivalence.js
+++ src/redux/sagas/updateEquivalence.js
@@ -2,7 +2,7 @@ import { put, call, takeLatest } from 'redux-saga/effects';
 import {
   APPROVE_EQUIVALENCE_START, APPROVE_EQUIVALENCE_COMPLETE, APPROVE_EQUIVALENCE_ERROR,
   REJECT_EQUIVALENCE_START, REJECT_EQUIVALENCE_COMPLETE, REJECT_EQUIVALENCE_ERROR,
-  SEARCH_REQUEST_START,
+  SEARCH_REQUEST_START, CONSULT_EQUIVALENCE_START, CONSULT_EQUIVALENCE_ERROR, CONSULT_EQUIVALENCE_COMPLETE,
 } from '../../consts/actionTypes';
 
 import apiCall from '../api';
@@ -27,7 +27,18 @@ export function* rejectEquivalence({ payload }) {
   }
 }
 
+export function* consultEquivalence({ payload }) {
+  try {
+    const results = yield call(apiCall, `/consult/requests/${payload.requestId}`, { payload }, null, 'POST');
+    yield put({ type: CONSULT_EQUIVALENCE_COMPLETE, results });
+    yield put({ type: SEARCH_REQUEST_START, payload });
+  } catch (error) {
+    yield put({ type: CONSULT_EQUIVALENCE_ERROR, error });
+  }
+}
+
 export default function* updateEquivalence() {
   yield takeLatest(APPROVE_EQUIVALENCE_START, approveEquivalence);
   yield takeLatest(REJECT_EQUIVALENCE_START, rejectEquivalence);
+  yield takeLatest(CONSULT_EQUIVALENCE_START, consultEquivalence);
 }
