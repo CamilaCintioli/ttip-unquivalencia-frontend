48133503f062ea3829dde69511a5c995ec8f3e71
camilacintioli <camila.cintioli@gmail.com>
2019-11-04 16:00:03 -0300

    Agrego accion para crear una materia

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux';
 import search from './search';
 import updateEquivalence from './updateEquivalence';
 import user from './user';
-import createRequest from './createRequest';
+import create from './create';
 import match from './match';
 
 const rootReducer = combineReducers({
   user,
   search,
   updateEquivalence,
-  createRequest,
+  create,
   match,
 });
 
