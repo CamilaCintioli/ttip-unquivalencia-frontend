16d69d58540adb74706d5cb252d1d8d33d11b916
camilacintioli <camila.cintioli@gmail.com>
2019-11-07 15:58:59 -0300

    Agrego informacion de las materias en la vista de la solicitud

--- src/component/Request/SubjectInformationDisplay.jsx
+++ src/component/Request/SubjectInformationDisplay.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(2),
     marginButton: theme.spacing(2),
-    width: '50%',
+    marginRight: theme.spacing(2),
+
   },
 
 }));
@@ -16,14 +20,23 @@ export default function SubjectInformationDisplay({ subject }) {
   const classes = useStyles();
   return (
     <Paper className={classes.paper}>
-      {subject.subject ? <h5>Materia: {subject.subject}</h5> : null}
-      {subject.career ? <h5>Carrera: {subject.career}</h5> : null}
-      {subject.yearPlan ? <h5>Año del plan: {subject.yearPlan}</h5> : null}
-      {subject.subjectWeeklyHours ? <h5>Horas semanales: {subject.subjectWeeklyHours}</h5> : null}
-      {subject.subjectTotalHours ? <h5>Horas totales: {subject.subjectTotalHours}</h5> : null}
-      {subject.courseMode ? <h5>Modalidad: {subject.courseMode}</h5> : null}
-      {subject.subjectCore ? <h5>Núcleo: {subject.subjectCore}</h5> : null}
-      {subject.credits ? <h5>Creditos: {subject.credits}</h5> : null}
+      {subject.subject ? <Typography align="center" variant="body1" gutterBottom>Materia: {subject.subject}</Typography> : null}
+      {subject.career ? <Typography align="center" variant="body1" gutterBottom>Carrera: {subject.career}</Typography> : null}
+      {subject.yearPlan ? <Typography align="center" variant="body1" gutterBottom>Año del plan: {subject.yearPlan}</Typography> : null}
+      {subject.subjectWeeklyHours ? <Typography align="center" variant="body1" gutterBottom>Horas semanales: {subject.subjectWeeklyHours}</Typography> : null}
+      {subject.subjectTotalHours ? <Typography align="center" variant="body1" gutterBottom>Horas totales: {subject.subjectTotalHours}</Typography> : null}
+      {subject.courseMode ? <Typography align="center" variant="body1" gutterBottom>Modalidad: {subject.courseMode}</Typography> : null}
+      {subject.subjectCore ? <Typography align="center" variant="body1" gutterBottom>Núcleo: {subject.subjectCore}</Typography> : null}
+      {subject.credits ? <Typography align="center" variant="body1" gutterBottom>Creditos: {subject.credits}</Typography> : null}
     </Paper>
   );
 }
+
+
+SubjectInformationDisplay.propTypes = {
+  subject: PropTypes.object,
+};
+
+SubjectInformationDisplay.defaultProps = {
+  subject: {},
+};
