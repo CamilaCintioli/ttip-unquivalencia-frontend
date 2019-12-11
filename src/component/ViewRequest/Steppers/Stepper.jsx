00e29323a3aa3615643dbeb530d292dbb1b94ed5
Fernando Nahuel Autalan <f.n.autalan@gmail.com>
2019-12-11 12:19:01 -0300

    active stepper

---
+++ src/component/ViewRequest/Steppers/Stepper.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import { map } from 'lodash';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseIcon from '@material-ui/icons/Close';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import { useParams } from 'react-router-dom';
+import Badge from '@material-ui/core/Badge';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function HorizontalNonLinearStepper({
+  changeStep, requests, level,
+}) {
+  const { requestId, subjectId } = useParams();
+  const classes = useStyles();
+  const getIcon = (state, active) => {
+    switch (state) {
+      case 'APROBADA':
+        return active ? (
+          <Badge color="primary" variant="dot">
+            <CheckCircleIcon color="primary" />
+          </Badge>
+        ) : <CheckCircleIcon color="primary" />;
+
+      case 'NEGADA':
+        return active ? (
+          <Badge color="primary" variant="dot">
+            <CloseIcon color="error" />
+          </Badge>
+        ) : <CloseIcon color="error" />;
+      case 'CONSULTA':
+        return active ? (
+          <Badge color="primary" variant="dot">
+            <SupervisedUserCircleIcon color="secondary" />
+          </Badge>
+        ) : <SupervisedUserCircleIcon color="secondary" />;
+      case 'GIRADA':
+        return active ? (
+          <Badge color="primary" variant="dot">
+            <CheckCircleIcon color="primary" />
+          </Badge>
+        ) : <CheckCircleIcon color="primary" />;
+      default:
+        return active ? (
+          <Badge color="primary" variant="dot">
+            <CheckBoxOutlineBlankIcon color="primary" />
+          </Badge>
+        ) : <CheckBoxOutlineBlankIcon color="primary" />;
+    }
+  };
+  const handleStep = (id) => () => changeStep(id);
+  const getName = (request) => (level ? request.subjectUnq : request.subjectOrigin);
+  const getId = (request) => (level ? request.requestId : request.subjectId);
+  const getParam = () => (level ? requestId : subjectId);
+  const active = (request) => parseInt(getId(request), 10) === parseInt(getParam(), 10);
+
+  return (
+    <Stepper
+      className={classes}
+      nonLinear
+      color="blue"
+    >
+      {map(requests, (request, index) => (
+        <Step key={index}>
+          <StepButton onClick={handleStep(getId(request))} completed icon={getIcon(request.equivalence, active(request))}>
+            {getName(request)}
+          </StepButton>
+        </Step>
+      ))}
+    </Stepper>
+  );
+}
